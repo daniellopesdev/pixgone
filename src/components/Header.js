50306b877e893c97dc29d900d1d7b5f49a0c9244
DanielL24 <daniel.lopes@wundermanthompson.com>
2025-06-23 15:54:46 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Header.css';
-import logoPix from '../logoPix.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <img src={logoPix} alt="pixGone logo" className="logo-img" />
+        <img src={process.env.PUBLIC_URL + '/logoPix.png'} alt="pixGone logo" className="logo-img" />
         <span className="logo-text">pixGone</span>
       </div>
     </header>
